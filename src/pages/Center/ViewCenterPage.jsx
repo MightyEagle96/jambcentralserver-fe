d80d46bee9d6474ad7eb993b343287261526bf0c
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-01-13 06:56:02 +0100

    made chanes

--- src/pages/Center/ViewCenterPage.jsx
+++ src/pages/Center/ViewCenterPage.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useState } from "react/cjs/react.development";
 import { httpService } from "../../services/services";
 
 export default function ViewCenterPage() {
