65fad889d3d0f8b868bd8cfab17a8b7dd05eafd5
Stephen-Ibe <saintkalson@gmail.com>
2022-01-23 03:09:30 +0100

    setup redux store

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { applyMiddleware, createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const middleware = [thunk];
+const persistConfig = {
+  key: "persist-config",
+  storage,
+};
+const initialState = {};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export const persistor = persistStore(store);
