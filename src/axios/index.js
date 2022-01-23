25f745355c0ed9e8f829f8d71d9a74c17b5bf118
Stephen-Ibe <saintkalson@gmail.com>
2022-01-23 04:22:53 +0100

    custom alerts and getErrorMessages

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,3 +1,34 @@
 import axios from "axios";
+import { history } from "../utils/History";
 
-const instance = axios.create({});
+const url =
+  process.env.REACT_APP_ENV === "production"
+    ? "https://jambcentralserver.herokuapp.com/jamb/"
+    : "http://localhost:8000/jamb/";
+
+const instance = axios.create({
+  baseURL: url,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    config.headers["Content-Type"] = "application/json";
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+const successHandler = (response) => {
+  return response;
+};
+
+const errorhandler = (error) => {
+  const status = error.response;
+  if (status === 401) {
+    triggerError("Your session has expired! Kindly Login again");
+    history.push("/");
+  }
+};
